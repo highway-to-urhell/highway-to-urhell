6080c832530c421e24b5176ca201cf7dead5f074
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2014-05-14 17:27:44 +0200

    add servlet3.0 annotation to make our servlet auto registered

--- h2hell-core/src/main/java/io/highway/to/urhell/servlet/H2hellServlet.java
+++ h2hell-core/src/main/java/io/highway/to/urhell/servlet/H2hellServlet.java
@@ -4,31 +4,31 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 /**
  * 
  * Servlet add in your web.xml
- *
+ * 
  */
+@WebServlet(name = "H2H", urlPatterns = { "/h2h" })
 public class H2hellServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
-	
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/plain");
-		PrintWriter out = response.getWriter();
-//		out.println(RegistryService.getInstance().executeServiceRegistry());
-		out.close();
-	}
+    private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        // out.println(RegistryService.getInstance().executeServiceRegistry());
+        out.close();
+    }
 
-	
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        doPost(request, response);
+    }
 
 }

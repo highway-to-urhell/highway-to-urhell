bbca906488a50bdcfe0ecb0db89c49b2e638ce19
nicopoirier <nicolas.npoirier@gmail.com>
2014-08-26 11:25:36 +0200

    add projet example

---
+++ h2h-example/h2hell-example-servlet3/src/main/java/io/highway/to/urhell/servlet/H2hellServlet.java
@@ -0,0 +1,36 @@
+package io.highway.to.urhell.servlet;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * 
+ * Servlet add in your web.xml
+ * 
+ */
+@WebServlet(value = "/h2h/*", name = "h2h-servlet")
+public class H2hellServlet extends HttpServlet {
+
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		//nothing
+	}
+	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		//nothing
+	}
+
+}

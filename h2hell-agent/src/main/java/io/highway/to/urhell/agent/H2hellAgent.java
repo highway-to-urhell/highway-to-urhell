1dc1e640aac47a6bcb1357ab59ff0b7e72cbcdf7
nicopoirier <nicolas.npoirier@gmail.com>
2015-02-11 18:07:25 +0100

    Persist in memory instrumentation

--- h2hell-agent/src/main/java/io/highway/to/urhell/agent/H2hellAgent.java
+++ h2hell-agent/src/main/java/io/highway/to/urhell/agent/H2hellAgent.java
@@ -1,18 +1,19 @@
 package io.highway.to.urhell.agent;
 
+import io.highway.to.urhell.CoreEngine;
 import io.highway.to.urhell.PluginUtils;
 import io.highway.to.urhell.transformer.AbstractLeechTransformer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.instrument.Instrumentation;
 
 public class H2hellAgent {
-    private static final Logger LOGGER = LoggerFactory.getLogger(H2hellAgent.class);
+    
 
     public static void premain(String agentArgs, Instrumentation inst) {
-        for (AbstractLeechTransformer transformer : PluginUtils.autodiscoverPlugin(AbstractLeechTransformer.class)) {
+    	for (AbstractLeechTransformer transformer : PluginUtils.autodiscoverPlugin(AbstractLeechTransformer.class)) {
             inst.addTransformer(transformer);
         }
+    	CoreEngine.getInstance().persistInMemory(inst);
+        
     }
 }

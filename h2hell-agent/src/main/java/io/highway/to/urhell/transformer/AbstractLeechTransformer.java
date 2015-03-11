ded763ddc1fc828bc60cc4e8a95f26c84e5bb219
nicopoirier <nicolas.npoirier@gmail.com>
2015-03-11 14:22:15 +0100

    fix import

--- h2hell-agent/src/main/java/io/highway/to/urhell/transformer/AbstractLeechTransformer.java
+++ h2hell-agent/src/main/java/io/highway/to/urhell/transformer/AbstractLeechTransformer.java
@@ -1,21 +1,18 @@
 package io.highway.to.urhell.transformer;
 
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtConstructor;
-import javassist.CtMethod;
-import javassist.LoaderClassPath;
-import javassist.NotFoundException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.FileNotFoundException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 import java.util.ArrayList;
 import java.util.List;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.LoaderClassPath;
+import javassist.NotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class AbstractLeechTransformer implements ClassFileTransformer {
 

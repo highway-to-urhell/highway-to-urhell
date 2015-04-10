f2c007a5ce874e045422ea1e4287099dcedd547d
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2015-04-10 19:21:19 +0200

    drop dead code

--- h2hell-core/src/main/java/io/highway/to/urhell/agent/H2hellAgent.java
+++ h2hell-core/src/main/java/io/highway/to/urhell/agent/H2hellAgent.java
@@ -1,17 +1,18 @@
 package io.highway.to.urhell.agent;
 
+import io.highway.to.urhell.PluginUtils;
 import io.highway.to.urhell.transformer.AbstractLeechTransformer;
 
 import java.lang.instrument.Instrumentation;
 
 public class H2hellAgent {
-    
+
 
     public static void premain(String agentArgs, Instrumentation inst) {
-    	for (AbstractLeechTransformer transformer : PluginUtils.autodiscoverPlugin(AbstractLeechTransformer.class)) {
+        for (AbstractLeechTransformer transformer : PluginUtils.autodiscoverPlugin(AbstractLeechTransformer.class)) {
             inst.addTransformer(transformer);
         }
-    	AgentService.getInstance().persistInMemory(inst);
-        
+        AgentService.getInstance().persistInMemory(inst);
+
     }
 }

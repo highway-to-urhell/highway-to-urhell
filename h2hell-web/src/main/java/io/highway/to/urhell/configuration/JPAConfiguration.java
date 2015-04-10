d0bbdf4cc784cc87eed192cad173c5f3b6c554a7
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2015-04-10 19:09:31 +0200

    cleanup

--- h2hell-web/src/main/java/io/highway/to/urhell/configuration/JPAConfiguration.java
+++ h2hell-web/src/main/java/io/highway/to/urhell/configuration/JPAConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -20,6 +21,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "io.highway.to.urhell.dao")
 public class JPAConfiguration {
 
     @Resource(name = "dataSource")

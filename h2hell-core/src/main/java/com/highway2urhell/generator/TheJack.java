df252f39fd9ff8416d320b9443bf6afdf33b70cf
nicopoirier <nicolas.npoirier@gmail.com>
2015-11-25 22:46:39 +0100

    remove filter and servlet. Agent asks information to H2HWeb Server. The mode local is delete

--- h2hell-core/src/main/java/com/highway2urhell/generator/TheJack.java
+++
@@ -1,9 +0,0 @@
-package com.highway2urhell.generator;
-
-import com.highway2urhell.service.LeechService;
-
-import java.util.Collection;
-
-public interface TheJack {
-	String generatePage(Collection<LeechService> collectionService);
-}
